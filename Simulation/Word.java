4e5f00437123d07906b6f5dbfdda8a5a1b91a06b
cym0n-20 <simon.gustafsson20@hotmail.com>
2015-04-15 14:16:38 +0200

    Ändrat word till abstract

--- Simulation/Word.java
+++ Simulation/Word.java
@@ -1,57 +1,16 @@
 package Simulation;
 
-import java.util.*;
-
-/**
- * 
- */
 public abstract class Word implements Operand {
-
-    /**
-     * 
-     */
-    public Word() {
-    }
-
-    /**
-     * @param Word w1 
-     * @param Word w2
-     */
-    public Word add(Word w1, Word w2) {
-        // TODO implement here
-    	return null;
-    }
-
-    /**
-     * @param Word w1
-     */
-    public boolean equalsTo(Word w1) {
-        // TODO implement here
-    	return true;
-    }
-
-    /**
-     * @param Memory memory
-     */
-    public Word get(Memory memory) {
-        // TODO implement here
-    	return null;
-    }
-
-    /**
-     * @param Word w1 
-     * @param Word w2
-     */
-    public Word mul(Word w1, Word w2) {
-        // TODO implement here
-    	return null;
-    }
-
-    /**
-     * 
-     */
-    public String toString() {
-       return "";
-    }
-
+	
+    public abstract Word add(Word word);
+ 
+    public abstract boolean equalsTo(Word w1);
+    
+    public abstract Word get(Memory memory);
+    
+    public abstract Word mul(Word w1, Word w2);
+    
+    @Override
+    public abstract String toString();
+    
 }
